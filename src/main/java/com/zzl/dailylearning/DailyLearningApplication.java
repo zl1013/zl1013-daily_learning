d120bdd83d0299cef778f98610c5e34195f177d5
zhaozl <zhaozl@133.cn>
2021-09-30 16:23:09 +0800

    update log info

--- src/main/java/com/zzl/dailylearning/DailyLearningApplication.java
+++ src/main/java/com/zzl/dailylearning/DailyLearningApplication.java
@@ -1,13 +1,32 @@
 package com.zzl.dailylearning;
 
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
+@Slf4j
 public class DailyLearningApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DailyLearningApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext context = SpringApplication.run(DailyLearningApplication.class, args);
+        Environment environment = context.getBean(Environment.class);
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = environment.getProperty("server.port");
+        String path = environment.getProperty("server.servlet.context-path");
+        if (StrUtil.isEmpty(path)) {
+            path = "";
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application  is running! Access URLs:\n\t" +
+                "Local访问网址: \t\thttp://localhost:" + port + path + "\n\t" +
+                "External访问网址: \thttp://" + ip + ":" + port + path + "\n\t" +
+                "----------------------------------------------------------");
     }
-
 }
